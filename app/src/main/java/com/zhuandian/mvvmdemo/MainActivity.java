56717a1cb586b96f04109b940753ac4ce8724033
xiedong <zx381010>
2018-09-13 17:38:11 +0800

    Activity中使用data binding

--- app/src/main/java/com/zhuandian/mvvmdemo/MainActivity.java
+++ app/src/main/java/com/zhuandian/mvvmdemo/MainActivity.java
@@ -1,13 +1,19 @@
 package com.zhuandian.mvvmdemo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zhuandian.mvvmdemo.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        mainBinding  = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        mainBinding.tvInfo.setText("Data Binding");
     }
 }
